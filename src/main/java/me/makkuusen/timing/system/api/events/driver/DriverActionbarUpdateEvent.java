bbb812555d6c66a0218f18be12d1379c84e06355
M2B5 <mbowers2205@gmail.com>
2026-03-19 22:02:13 +0000

    Fix actionbar event

--- src/main/java/me/makkuusen/timing/system/api/events/driver/DriverActionbarUpdateEvent.java
+++ src/main/java/me/makkuusen/timing/system/api/events/driver/DriverActionbarUpdateEvent.java
@@ -1,11 +1,13 @@
 package me.makkuusen.timing.system.api.events.driver;
 
+import lombok.Getter;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+@Getter
 public class DriverActionbarUpdateEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
     private final Player player;
